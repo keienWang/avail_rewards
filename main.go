72046982b6f67bf50e90e42c25ffa490d246259d
keienWang <42377006+keienWang@users.noreply.github.com>
2024-04-19 15:05:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bytes"
 	"crypto/ecdsa"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/big"
 	"net/http"
 	"time"
 
@@ -13,16 +15,34 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// Define the URL for the web service
-const urlLogin = "https://claim-api.availproject.org/check-rewards"
+func getSign(privateKey *ecdsa.PrivateKey, time int64) []byte {
 
-// Define the Infura RPC endpoint
-const RPC = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
+	// privateKey, err := crypto.HexToECDSA(priv)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-func main() {
+	// timestamp := time.Now().Unix()
+	message := fmt.Sprintf("Greetings from Avail!\n\nSign this message to check your eligibility. This signature will not cost you any fees.\n\nTimestamp: %d", time)
 
-	// Define your private keys
-	privateKeys := []string{"YOUR_PRIVATE_KEY_1", "YOUR_PRIVATE_KEY_2"}
+	fullMessage := fmt.Sprintf("\x19Ethereum Signed Message:\n%d%s", len(message), message)
+	hash := crypto.Keccak256Hash([]byte(fullMessage))
+	signatureBytes, err := crypto.Sign(hash.Bytes(), privateKey)
+	if err != nil {
+		fmt.Println("sign error!")
+	}
+	signatureBytes[64] += 27
+
+	return signatureBytes
+}
+
+func GetAvail(privateKey *ecdsa.PrivateKey) {
+
+	// Define the URL for the web service
+	const urlLogin = "https://claim-api.availproject.org/check-rewards"
+
+	// Define the Infura RPC endpoint
+	const RPC = "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID"
 
 	// Create an Ethereum client
 	// client, err := ethclient.Dial(RPC)
@@ -32,62 +52,78 @@ func main() {
 	// }
 
 	// Loop through each private key
-	for _, privateKey := range privateKeys {
-		// Convert the private key string to a crypto.PrivateKey
-		privateKeyBytes, err := crypto.HexToECDSA(privateKey)
-		if err != nil {
-			fmt.Println("Failed to parse private key:", err)
-			continue
-		}
 
-		// Generate the Ethereum address from the private key
-		publicKey := privateKeyBytes.Public()
-		publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-		if !ok {
-			fmt.Println("Failed to generate public key from private key")
-			continue
-		}
-		fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	// Convert the private key string to a crypto.PrivateKey
+	// privateKeyBytes, err := crypto.HexToECDSA(priv)
+	// if err != nil {
+	// 	fmt.Println("Failed to parse private key:", err)
+	// 	return
+	// }
 
-		// Sign the message
-		timestamp := time.Now().Unix()
-		message := fmt.Sprintf("Greetings from Avail!\n\nSign this message to check your eligibility. This signature will not cost you any fees.\n\nTimestamp: %d", timestamp)
-		messageHash := crypto.Keccak256([]byte(message))
-		signature, err := crypto.Sign(messageHash, privateKeyBytes)
-		if err != nil {
-			fmt.Println("Failed to sign message:", err)
-			continue
-		}
-
-		// Prepare the data for the web service
-		requestData := map[string]interface{}{
-			"account":       fromAddress.Hex(),
-			"signedMessage": common.Bytes2Hex(signature),
-			"timestamp":     timestamp,
-			"type":          "ETHEREUM",
-		}
-		requestDataBytes, err := json.Marshal(requestData)
-		if err != nil {
-			fmt.Println("Failed to marshal request data:", err)
-			continue
-		}
-
-		// Make the HTTP POST request
-		resp, err := http.Post(urlLogin, "application/json", bytes.NewBuffer(requestDataBytes))
-		if err != nil {
-			fmt.Println("Failed to make HTTP request:", err)
-			continue
-		}
-		defer resp.Body.Close()
-
-		// Read the response body
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Failed to read response body:", err)
-			continue
-		}
-
-		// Print the response
-		fmt.Println("Response:", string(body))
+	// Generate the Ethereum address from the private key
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		fmt.Println("Failed to generate public key from private key")
+		return
 	}
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	// Sign the message
+	timestamp := time.Now().Unix()
+
+	signature := getSign(privateKey, timestamp)
+
+	// Prepare the data for the web service
+	requestData := map[string]interface{}{
+		"account":       fromAddress.Hex(),
+		"signedMessage": "0x" + common.Bytes2Hex(signature),
+		"timestamp":     timestamp,
+		"type":          "ETHEREUM",
+	}
+	// fmt.Println(requestData)
+	requestDataBytes, err := json.Marshal(requestData)
+	if err != nil {
+		fmt.Println("Failed to marshal request data:", err)
+		return
+
+	}
+
+	// Make the HTTP POST request
+	resp, err := http.Post(urlLogin, "application/json", bytes.NewBuffer(requestDataBytes))
+	if err != nil {
+		fmt.Println("Failed to make HTTP request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Failed to read response body:", err)
+		return
+	}
+
+	// Print the response
+	fmt.Println("Response:", string(body))
+
+}
+
+func main() {
+	startPrivateKey := big.NewInt(0)
+	for {
+		startPrivateKey.Add(startPrivateKey, big.NewInt(1))
+		privateKeyBytes := startPrivateKey.Bytes()
+
+		// 为字节数组填充前导0，以确保其长度为32字节
+		paddedPrivateKey := append(make([]byte, 32-len(privateKeyBytes)), privateKeyBytes...)
+		privateKeyECDSA, err := crypto.ToECDSA(paddedPrivateKey)
+		if err != nil {
+			fmt.Printf("Failed to create private key: %v\n", err)
+			return
+		}
+		fmt.Println(hex.EncodeToString(paddedPrivateKey))
+		GetAvail(privateKeyECDSA)
+	}
+
 }
